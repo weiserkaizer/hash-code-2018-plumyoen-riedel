ac542d1a12e025441a630b7ac69b78d42783aa00
Cromnir <kevin.plumyoen@laposte.net>
2018-03-01 19:15:27 +0100

    Début Challenge

---
+++ Ride.h
@@ -0,0 +1,13 @@
+#ifndef RIDE_H
+#define RIDE_H
+
+#include <iostream>
+
+class Ride{
+private:
+	
+public:
+
+}
+
+#endif
---
+++ Vehicle.h
@@ -0,0 +1,13 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <iostream>
+
+class Vehicle{
+private:
+	
+public:
+
+}
+
+#endif
